39e62d6ad8349f7e17ca4936c2988688e1e2185d
khOsman <khanosmansh@gmail.com>
2020-04-22 00:57:31 +0600

    Updates

---
+++ precache-manifest.468d0680c17ae61187523fad84700119.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b78901c4c51582cc536f641eebd5483c",
+    "url": "/corona_data_vs/index.html"
+  },
+  {
+    "revision": "0981462b8fd37e3f8048",
+    "url": "/corona_data_vs/static/css/main.502c79b0.chunk.css"
+  },
+  {
+    "revision": "4b11afe5c727a926b4e9",
+    "url": "/corona_data_vs/static/js/2.b74d7f42.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona_data_vs/static/js/2.b74d7f42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0981462b8fd37e3f8048",
+    "url": "/corona_data_vs/static/js/main.a52dd1aa.chunk.js"
+  },
+  {
+    "revision": "a27489645d679300f0c6",
+    "url": "/corona_data_vs/static/js/runtime-main.3cf31aaf.js"
+  },
+  {
+    "revision": "ff66dce012b34ac292184919d1326f2b",
+    "url": "/corona_data_vs/static/media/corona2.ff66dce0.png"
+  }
+]);
